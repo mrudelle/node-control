039c05a75ce28d34498285d20eea37d80936da1b
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2014-11-18 13:38:06 +0100

    added orientation feedback

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
-
 (function(){
 	var app = angular.module('nodeControlApp', []);
 
-	app.controller('appController', function(){
-		this.msg = "Hello, World";
-	});
+	app.controller('appController', [ '$scope', '$window', function($scope, $window){
+		
+		$scope.msg = "Hello, World";
+		$scope.alpha = 3;
+		$scope.beta = 3;
+		$scope.gamma = 3;
+
+		$scope.newOrientation = function(event) {
+			$scope.beta = Math.trunc(event.beta);
+			$scope.gamma = Math.trunc(event.gamma);
+			$scope.alpha = Math.trunc(event.alpha);
+			$scope.$apply()
+		};
+
+		$window.addEventListener("deviceorientation", $scope.newOrientation);
+	
+	}]);
+
 })();
