a5f6224764fbee3ecd42503c1c501f461cadac15
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2014-11-20 08:39:14 +0100

    better initailisation

--- js/app.js
+++ js/app.js
@@ -1,22 +1,42 @@
 (function(){
-	var app = angular.module('nodeControlApp', []);
+	var app = angular.module('nodeControlApp', [])
 
 	app.controller('appController', [ '$scope', '$window', function($scope, $window){
 		
-		$scope.msg = "Hello, World";
-		$scope.alpha = 3;
-		$scope.beta = 3;
-		$scope.gamma = 3;
+		$scope.msg = "Hello, World"
+
+		$scope.orientation = {}
+		$scope.orientation.alpha = 0
+		$scope.orientation.beta = 0
+		$scope.orientation.gamma = 0
+
+		$scope.oInit = {}
+		$scope.oInit.alpha = 0
+		$scope.oInit.beta = 0
+		$scope.oInit.gamma = 0
+		$scope.oInit.init = 1
+
 
 		$scope.newOrientation = function(event) {
-			$scope.beta = Math.trunc(event.beta);
-			$scope.gamma = Math.trunc(event.gamma);
-			$scope.alpha = Math.trunc(event.alpha);
+			$scope.orientation.beta = Math.trunc(event.beta)
+			$scope.orientation.gamma = Math.trunc(event.gamma)
+			$scope.orientation.alpha = Math.trunc(event.alpha)
+
+			if(!$scope.oInit.init == 0) {
+				$scope.oInit.init = 0
+				$scope.oInit.beta = $scope.orientation.beta
+				$scope.oInit.gamma = $scope.orientation.gamma
+				$scope.oInit.alpha = $scope.orientation.alpha
+			}
+
 			$scope.$apply()
 		};
 
-		$window.addEventListener("deviceorientation", $scope.newOrientation);
+		$window.addEventListener("deviceorientation", $scope.newOrientation)
 	
+		$scope.newInit = function() {
+			$scope.oInit.init = 1
+		}
 	}]);
 
 })();
