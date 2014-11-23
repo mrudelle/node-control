a678e3fbba523b9f34096cc75812ce1924a2f5cb
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2014-11-24 00:55:40 +0100

    Moving in Maps WORKS !!!

--- static/js/interface-maps.js
+++ static/js/interface-maps.js
@@ -2,9 +2,20 @@
 
 	var app = angular.module('mapInterfaceApp', [ 'remoteControlModule' ])
 
-	app.controller('appController', [ '$scope', '$http', 'MonitorService', function($scope, $http, monitorService){
+	app.controller('appController', [ '$scope', '$http', '$interval', 'MonitorService', function($scope, $http, $interval, monitorService){
 		
 		$scope.monitor = monitorService;
+
+		$scope.lat = 46.508
+		$scope.lon = 6.553
+
+		$scope.delta = 0.1;
+
+		$interval(function() {
+			
+			$scope.lat -= $scope.delta * Math.abs($scope.monitor.orientation.beta) * $scope.monitor.orientation.beta /(Math.pow(50,2))
+			$scope.lon += $scope.delta * Math.abs($scope.monitor.orientation.gamma) * $scope.monitor.orientation.gamma /(Math.pow(50,2))
+		}, 100);
 
 	}]);
 
