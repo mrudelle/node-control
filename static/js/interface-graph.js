ee505044cb110891e782d00cfdd64f7d33df769e
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2014-12-22 22:28:05 +0100

    change chart API for a lighter one

--- static/js/interface-graph.js
+++ static/js/interface-graph.js
@@ -2,18 +2,21 @@
 
 	var app = angular.module('graphInterfaceApp', [ 'remoteControlModule' ])
 
-	app.controller('appController', [ '$scope', '$http', 'MonitorService', function($scope, $http, monitorService){
+	app.controller('appController', [ '$scope', '$http', '$window', 'MonitorService', function($scope, $http, $window, monitorService){
 		
 		$scope.monitor = monitorService;
+
+		$scope.initDate = new Date().getTime();
 
 		$scope.monitor.onEvent(function (event)
 		{
 			if (event.type == "orientation")
 			{
 				// TODO: replace with time shipped with information
-				$scope.chart.series[0].addPoint([new Date().getTime(), event.alpha], true, false);
-				$scope.chart.series[1].addPoint([new Date().getTime(), event.beta], true, false);
-				$scope.chart.series[2].addPoint([new Date().getTime(), event.gamma], true, false);
+				myLiveChart.addData([((event.alpha+180)%360), ((event.beta+180)%360), ((event.gamma+180)%360)], new Date().getTime() - $scope.initDate);
+
+				if(myLiveChart.datasets[0].points.length > 20)
+					myLiveChart.removeData();
 			}
 			else
 			{
@@ -23,40 +26,60 @@
 
 		// Instantiate graph
 
-		$scope.chart = new Highcharts.Chart({
-	        chart: {
-	            renderTo: 'chart',
-	            type: 'line'
-	        },
-	        // title: {
-	        //     text: 'Fruit Consumption'
-	        // },
-	        xAxis: {
-	            title: {
-	            	text: 'Time (ms)'
-	            }
-	        },
-	        yAxis: {
-	            title: {
-	                text: 'Angle'
-	            }
-	        },
-	        series: 
-	        [
-		        {
-		        	name: 'Alpha',
-			        data: []
-		    	},
-		    	{
-		    		name: 'Beta',
-			        data: []
-		    	},
-		    	{
-		    		name: 'Gamma',
-			        data: []
-		    	}
-	    	]
-	    });
+		var canvas = $window.document.getElementById('chart');
+		var ctx = canvas.getContext('2d');
+		var startingData = {
+		      labels: [1,2],
+		      datasets: [
+		          {
+		          	  label: "Alpha",
+		              fillColor: "rgba(205,151,187,0.2)",
+		              strokeColor: "rgba(205,151,187,1)",
+		              pointColor: "rgba(205,151,187,1)",
+		              pointStrokeColor: "#fff",
+		              data: [1,2]
+		          },
+		          {
+		          	  label: "Beta",
+		              fillColor: "rgba(151,187,205,0.2)",
+		              strokeColor: "rgba(151,187,205,1)",
+		              pointColor: "rgba(151,187,205,1)",
+		              pointStrokeColor: "#fff",
+		              data: [1,2]
+		          },
+		          {
+		          	  label: "Gamma",
+		              fillColor: "rgba(151,205,187,0.2)",
+		              strokeColor: "rgba(151,205,187,1)",
+		              pointColor: "rgba(151,205,187,1)",
+		              pointStrokeColor: "#fff",
+		              data: [1,2]
+		          }
+		      ]
+		    };
+
+		var win = angular.element($window); 
+
+		var canvasResize = function(){
+		    var w = $window.innerWidth;
+		    var h = $window.innerHeight;
+
+		    canvas.width = w;
+		    canvas.style.width = w;
+		    canvas.height = h; 
+		    canvas.style.height = h;
+		}
+		win.bind("resize", canvasResize);
+		canvasResize();
+
+		// Reduce the animation steps for demo clarity.
+		var myLiveChart = new Chart(ctx).Line(startingData, 
+			{
+				responsive: true,
+				animation: false,
+				animationSteps: 2,
+				bezierCurve: false
+			});
 
 	}]);
 
